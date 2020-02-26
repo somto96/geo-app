723d1471a29ff64fe3d501fb972c2beb248d5331
Somto96 <motivatedsomt@gmail.com>
2020-02-26 19:21:02 +0100

    production  changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,60 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
 
-Vue.use(VueRouter)
+import Vuikit from "vuikit";
+import VuikitIcons from "@vuikit/icons";
+
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
+import Mapview from "../views/Mapview.vue";
+import Visualize from "../views/Visualize.vue";
+
+import "@vuikit/theme";
+
+Vue.use(VueRouter);
+Vue.use(Vuikit);
+Vue.use(VuikitIcons);
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/register",
+    name: "Register",
+    component: Register
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/mapview",
+    name: "Mapview",
+    component: Mapview
+  },
+  {
+    path: "/visualize",
+    name: "Visualize",
+    component: Visualize
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
